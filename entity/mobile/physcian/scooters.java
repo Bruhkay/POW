89cf20722a5c444965f58500cd6b157c5a66163e
Atakan <atakanka350@gmail.com>
2024-03-13 15:11:17 +0300

    Name change try
    Let's see if these changes will stick

--- entity/mobile/physcian/scooters.java
+++
@@ -1,9 +0,0 @@
-package entity.mobile.physcian;
-public class Scooters extends Nurses {
-    
-    final protected int speed = 5;
-
-    public Scooters(String name){
-        super(name);
-    }
-}
